0a9f77d5c39a957ffe19c280bcb5849b8db6aa26
David Williams <burmesedragon@att.net>
2020-06-18 17:09:09 -0700

    Initial commit Set up opening screen, setting background color and sign in/log in buttons

---
+++ Wunderlist2/Wunderlist2/WunderlistSignInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WunderlistSignInViewController.swift
+//  Wunderlist2
+//
+//  Created by David Williams on 6/18/20.
+//  Copyright © 2020 david williams. All rights reserved.
+//
+
+import UIKit
+
+class WunderlistSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
